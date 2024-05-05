5c5fcb9fa4a7f7d5d074b6e395ac388736614aeb
Jaxson Keenes <jaxsonkeenes@gmail.com>
2024-05-05 19:57:41 +1000

    Adding Shoal Cave

---
+++ Emerald/Content/Scripts/Markers/ShoalCave.js
@@ -0,0 +1,23 @@
+var currentMap = "Shoal Cave";
+
+markerSet(-178, 391, "Overworld", entranceIcon, currentMap);
+
+
+markerSet(-150, 401, "Big Pearl", itemIcon, currentMap);
+markerSet(-197, 392, "Rare Candy", itemIcon, currentMap);
+
+markerSet(-192, 372, "Shoal Shell", itemIcon, currentMap);
+markerSet(-196, 382, "Shoal Shell", itemIcon, currentMap);
+markerSet(-193, 407, "Shoal Shell", itemIcon, currentMap);
+markerSet(-203, 407, "Shoal Shell", itemIcon, currentMap);
+
+markerSet(-191, 397, "Shoal Salt", itemIcon, currentMap);
+markerSet(-209, 380, "Shoal Salt", itemIcon, currentMap);
+markerSet(-233, 377, "Shoal Salt", itemIcon, currentMap);
+markerSet(-224, 406, "Shoal Salt", itemIcon, currentMap);
+
+markerSet(-234, 379, "Ice Heal", itemIcon, currentMap);
+
+markerSet(-212, 425, "Never-Melt Ice", itemIcon, currentMap);
+
+markerSet(-199, 425, "TM29 (Psychic)", tmIcon, currentMap);
---
+++ Emerald/Content/Scripts/Markers/ShoalCaveVector.js
@@ -0,0 +1,446 @@
+var ShoalCaveVector = {
+    "type": "FeatureCollection",
+    "features": [{
+            "type": "Feature",
+            "properties": {
+                "name": "Entrance Room",
+                "world": "Overworld",
+                "Pokémon": {
+                    "Cave": [{
+                            "name": "Zubat",
+                            "area": "Walking",
+                            "levels": "26, 28, 30, 32",
+                            "rate": '45%',
+                        },
+                        {
+                            "name": "Golbat",
+                            "area": "Walking",
+                            "levels": "32",
+                            "rate": '5%',
+                        },
+                        {
+                            "name": "Spheal",
+                            "area": "Walking",
+                            "levels": "26, 28, 30, 32",
+                            "rate": '50%',
+                        },
+                    ],
+                    "Surfing": [
+
+                        {
+                            "name": "Tentacool",
+                            "area": "Surfing",
+                            "levels": "5-35",
+                            "rate": '60%',
+                        },
+                        {
+                            "name": "Zubat",
+                            "area": "Surfing",
+                            "levels": "5-35",
+                            "rate": '30%',
+                        },
+                        {
+                            "name": "Spheal",
+                            "area": "Surfing",
+                            "levels": "25-35",
+                            "rate": '10%',
+                        },
+                    ],
+                    "Fishing": [
+
+                        {
+                            "name": "Tentacool",
+                            "area": "Old Rod",
+                            "levels": "5-10",
+                            "rate": '30%',
+                        },
+                        {
+                            "name": "Magikarp",
+                            "area": "Old Rod",
+                            "levels": "5-10",
+                            "rate": '70%',
+                        },
+                        {
+                            "name": "Tentacool",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '20%',
+                        },
+                        {
+                            "name": "Magikarp",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '60%',
+                        },
+                        {
+                            "name": "Wailmer",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '20%',
+                        },
+                        {
+                            "name": "Wailmer",
+                            "area": "Super Rod",
+                            "levels": "20-45",
+                            "rate": '100%',
+                        },
+                    ]
+                },
+            },
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        getCordfromLoc(-146, 370),
+                        getCordfromLoc(-146, 405),
+                        getCordfromLoc(-181, 405),
+                        getCordfromLoc(-181, 370),
+                    ]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Inner room",
+                "world": "Overworld",
+                "Pokémon": {
+                    "Cave": [{
+                            "name": "Zubat",
+                            "area": "Walking",
+                            "levels": "26, 28, 30, 32",
+                            "rate": '45%',
+                        },
+                        {
+                            "name": "Golbat",
+                            "area": "Walking",
+                            "levels": "32",
+                            "rate": '5%',
+                        },
+                        {
+                            "name": "Spheal",
+                            "area": "Walking",
+                            "levels": "26, 28, 30, 32",
+                            "rate": '50%',
+                        },
+                    ],
+                    "Surfing": [
+
+                        {
+                            "name": "Tentacool",
+                            "area": "Surfing",
+                            "levels": "5-35",
+                            "rate": '60%',
+                        },
+                        {
+                            "name": "Zubat",
+                            "area": "Surfing",
+                            "levels": "5-35",
+                            "rate": '30%',
+                        },
+                        {
+                            "name": "Spheal",
+                            "area": "Surfing",
+                            "levels": "25-35",
+                            "rate": '10%',
+                        },
+                    ],
+                    "Fishing": [
+
+                        {
+                            "name": "Tentacool",
+                            "area": "Old Rod",
+                            "levels": "5-10",
+                            "rate": '30%',
+                        },
+                        {
+                            "name": "Magikarp",
+                            "area": "Old Rod",
+                            "levels": "5-10",
+                            "rate": '70%',
+                        },
+                        {
+                            "name": "Tentacool",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '20%',
+                        },
+                        {
+                            "name": "Magikarp",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '60%',
+                        },
+                        {
+                            "name": "Wailmer",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '20%',
+                        },
+                        {
+                            "name": "Wailmer",
+                            "area": "Super Rod",
+                            "levels": "20-45",
+                            "rate": '100%',
+                        },
+                    ]
+                },
+            },
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        getCordfromLoc(-182, 365),
+                        getCordfromLoc(-182, 411),
+                        getCordfromLoc(-220, 411),
+                        getCordfromLoc(-220, 365),
+                    ]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Stairs room",
+                "world": "Overworld",
+                "Pokémon": {
+                    "Cave": [{
+                            "name": "Zubat",
+                            "area": "Walking",
+                            "levels": "26, 28, 30, 32",
+                            "rate": '45%',
+                        },
+                        {
+                            "name": "Golbat",
+                            "area": "Walking",
+                            "levels": "32",
+                            "rate": '5%',
+                        },
+                        {
+                            "name": "Spheal",
+                            "area": "Walking",
+                            "levels": "26, 28, 30, 32",
+                            "rate": '50%',
+                        },
+                    ],
+                    "Surfing": [
+
+                        {
+                            "name": "Tentacool",
+                            "area": "Surfing",
+                            "levels": "5-35",
+                            "rate": '60%',
+                        },
+                        {
+                            "name": "Zubat",
+                            "area": "Surfing",
+                            "levels": "5-35",
+                            "rate": '30%',
+                        },
+                        {
+                            "name": "Spheal",
+                            "area": "Surfing",
+                            "levels": "25-35",
+                            "rate": '10%',
+                        },
+                    ],
+                    "Fishing": [
+
+                        {
+                            "name": "Tentacool",
+                            "area": "Old Rod",
+                            "levels": "5-10",
+                            "rate": '30%',
+                        },
+                        {
+                            "name": "Magikarp",
+                            "area": "Old Rod",
+                            "levels": "5-10",
+                            "rate": '70%',
+                        },
+                        {
+                            "name": "Tentacool",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '20%',
+                        },
+                        {
+                            "name": "Magikarp",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '60%',
+                        },
+                        {
+                            "name": "Wailmer",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '20%',
+                        },
+                        {
+                            "name": "Wailmer",
+                            "area": "Super Rod",
+                            "levels": "20-45",
+                            "rate": '100%',
+                        },
+                    ]
+                },
+            },
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        getCordfromLoc(-221, 365),
+                        getCordfromLoc(-221, 386),
+                        getCordfromLoc(-236, 386),
+                        getCordfromLoc(-236, 365),
+                    ]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Lower room",
+                "world": "Overworld",
+                "Pokémon": {
+                    "Cave": [{
+                            "name": "Zubat",
+                            "area": "Walking",
+                            "levels": "26, 28, 30, 32",
+                            "rate": '45%',
+                        },
+                        {
+                            "name": "Golbat",
+                            "area": "Walking",
+                            "levels": "32",
+                            "rate": '5%',
+                        },
+                        {
+                            "name": "Spheal",
+                            "area": "Walking",
+                            "levels": "26, 28, 30, 32",
+                            "rate": '50%',
+                        },
+                    ],
+                    "Surfing": [
+
+                        {
+                            "name": "Tentacool",
+                            "area": "Surfing",
+                            "levels": "5-35",
+                            "rate": '60%',
+                        },
+                        {
+                            "name": "Zubat",
+                            "area": "Surfing",
+                            "levels": "5-35",
+                            "rate": '30%',
+                        },
+                        {
+                            "name": "Spheal",
+                            "area": "Surfing",
+                            "levels": "25-35",
+                            "rate": '10%',
+                        },
+                    ],
+                    "Fishing": [
+
+                        {
+                            "name": "Tentacool",
+                            "area": "Old Rod",
+                            "levels": "5-10",
+                            "rate": '30%',
+                        },
+                        {
+                            "name": "Magikarp",
+                            "area": "Old Rod",
+                            "levels": "5-10",
+                            "rate": '70%',
+                        },
+                        {
+                            "name": "Tentacool",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '20%',
+                        },
+                        {
+                            "name": "Magikarp",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '60%',
+                        },
+                        {
+                            "name": "Wailmer",
+                            "area": "Good Rod",
+                            "levels": "10-30",
+                            "rate": '20%',
+                        },
+                        {
+                            "name": "Wailmer",
+                            "area": "Super Rod",
+                            "levels": "20-45",
+                            "rate": '100%',
+                        },
+                    ]
+                },
+            },
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        getCordfromLoc(-221, 387),
+                        getCordfromLoc(-221, 418),
+                        getCordfromLoc(-235, 418),
+                        getCordfromLoc(-235, 387),
+                    ]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Ice Room",
+                "world": "Overworld",
+                "Pokémon": {
+                    "Cave": [
+                        {
+                            "name": "Zubat",
+                            "area": "Walking",
+                            "levels": "26, 28, 30",
+                            "rate": '40%',
+                        },
+                        {
+                            "name": "Golbat",
+                            "area": "Walking",
+                            "levels": "30, 32",
+                            "rate": '5%',
+                        },
+                        {
+                            "name": "Snorunt",
+                            "area": "Walking",
+                            "levels": "26, 28, 30",
+                            "rate": '10%',
+                        },
+                        {
+                            "name": "Spheal",
+                            "area": "Walking",
+                            "levels": "26, 28, 30, 32",
+                            "rate": '45%',
+                        },
+                    ],
+                },
+            },
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        getCordfromLoc(-190, 412),
+                        getCordfromLoc(-190, 432),
+                        getCordfromLoc(-220, 432),
+                        getCordfromLoc(-220, 412),
+                    ]
+                ]
+            }
+        }
+    ]
+};
